7f6ec3f03f41143392e59b80cb8d2d1d8c75f063
patrykkrol98 <patryk.krol.98@gmail.com>
2022-02-24 15:29:02 +0100

    added modal window w/ description and styling

--- src/app/sights-list/sights-list.component.ts
+++ src/app/sights-list/sights-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { SightseeingPoint } from '../models/sightseeing-point';
 import { SightsService } from '../services/sights.service';
 
@@ -10,13 +11,19 @@ import { SightsService } from '../services/sights.service';
 export class SightsListComponent implements OnInit {
 
   sights: SightseeingPoint[];
+  selectedSight: SightseeingPoint;
 
-  constructor(private sightsService: SightsService) {
+  constructor(private sightsService: SightsService, private modalService: NgbModal) {
   }
 
   ngOnInit(): void {
     this.sightsService.getSights().subscribe(sights => {
       this.sights = sights;
     });
+  }
+
+  open(content: any, sight: SightseeingPoint) {
+    this.selectedSight = sight;
+    this.modalService.open(content);
   }
 }
